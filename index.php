fe3bb55ad6abf9d34f8f176924456033cc725e89
damien-simplon <damien.bellec12@gmail.com>
2019-08-26 16:44:27 +0200

    la nav est dans un include header maintenant

--- index.php
+++ index.php
@@ -11,41 +11,9 @@
 
     <body>
     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Sweat</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">T-shirt</a>
-                </li>                
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Baskets</a>
-                </li>                
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Homme</a>
-                </li>                
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Femme</a>
-                </li>                
-            </ul>
-        </div>
-
-        <ul class="navbar-nav d-flex flex-row">
-            <li class="nav-item p-2"><a class="nav-link" href="./connexion.php">Connexion</a></li>
-            <li class="nav-item p-2"><a class="nav-link" href="./inscription.php">Inscription</a></li>
-        </ul>
-    </nav>
+    <?php
+        include "./include/header.inc.html";
+    ?>
 
     <?php 
         include "./include/content.inc.html";
