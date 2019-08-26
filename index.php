1a083b81fceba771cccd96d386ec16df77c368f7
lepretre <benjamin.lepretre.pro@gmail.com>
2019-08-26 14:36:24 +0200

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
-<link rel="stylesheet" href="css/footer.css" type="text/css" /><div class="footer">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="fr">
 
-    <div class="middleFoot">
-        <a href="#"><p>Contact</p></a>
-        <a href="#"><p>À propos</p></a>
-        <a href="#"><p>Mentions légales</p></a>
-    </div>
+    <head>
 
-    <div class="FAQ">
-        <a href="#"><p>Questions fréquentes</p></a>
-    </div>
+        <?php
+            include "./include/head.inc.html"
+        ?>
 
-    <div class="reseaux">
-   
-               <a href="#"><i class="fab fa-facebook"></i></a>
-     
-                <a href="#"><i class="fab fa-instagram"></i></a>
-     
-                <a href="#"><i class="fab fa-twitter-square"></i></a>
-    </div>
-</div>
+    </head>
+
+    <body>
+        
+        <?php
+            include "./include.footer.inc.html"
+        ?>
+
+    </body>
+
+</html>
