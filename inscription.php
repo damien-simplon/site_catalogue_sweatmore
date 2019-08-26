dd5ba3b2ab7f69b9f211e19546ac54bcc8b1a65f
damien-simplon <damien.bellec12@gmail.com>
2019-08-26 13:48:01 +0200

    ajout du front de l'inscription.php

---
+++ inscription.php
@@ -0,0 +1,52 @@
+<?php
+
+    include 'head.inc.html';
+
+    echo'
+        <div class="container">
+            <h3 class="text-center">Bonjour</h3>
+            <div class="text-center">
+               <button type="submit" name="connexion" class="btn btn-success">Se connecter</button>
+            </div></br>
+
+                <h3 class="text-center">Créer un nouveau compte</h3>
+
+            <form action="treatment.php" method="post">
+                <div class="form-group">
+                    <label for="prenom">Prénom</label>
+                    <input type="text" name="prenom" class="form-control" id="prenom" placeholder="Prénom" required />
+                </div>
+                <div class="form-group">
+                    <label for="nom">Nom</label>
+                    <input type="text" name="nom" class="form-control" id="nom" placeholder="Nom" required />
+                </div>
+                <div class="form-group">
+                    <label for="date_de_naissance">Date de naissance</label>
+                    <input type="date" name="date_de_naissance" class="form-control" id="date_de_naissance" min="1900-01-01" max="2020-01-01" required />
+                </div>
+                <div class="form-group">
+                    <label for="email">Adresse email</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="email@gmail.com" required />
+                </div>
+                <div class="form-group">
+                    <label for="mdp">Mot de passe</label>
+                    <input type="password" name="mdp" class="form-control" id="mdp" placeholder="Mot de passe" required />
+                </div>
+                <div class="form-group">
+                    <label for="sexe">Sexe</label>
+                    </br>
+                    <input type="radio" name="femme" id="femme" checked/>
+                    <label for="femme">Femme</label>
+                    <input type="radio" name="homme" id="homme"/>
+                    <label for="homme">Homme</label>
+                </div>
+
+                <div class="text-center">
+                    <button type="submit" name="submit_new_account" class="btn btn-success">Valider</button>
+                </div>
+            </form>
+
+            <div class="dropdown-divider"></div>
+
+        </div>';
+?>
