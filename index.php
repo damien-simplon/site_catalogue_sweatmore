c9ae70f1219f9710d504b549415995809601ad27
Justine Lemaire <lemairejustine01@gmail.com>
2019-08-26 14:16:07 +0200

    footer soit index.php et footer.css

---
+++ index.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="css/footer.css" type="text/css" /><div class="footer">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+    <div class="middleFoot">
+        <a href="#"><p>Contact</p></a>
+        <a href="#"><p>À propos</p></a>
+        <a href="#"><p>Mentions légales</p></a>
+    </div>
+
+    <div class="FAQ">
+        <a href="#"><p>Questions fréquentes</p></a>
+    </div>
+
+    <div class="reseaux">
+   
+               <a href="#"><i class="fab fa-facebook"></i></a>
+     
+                <a href="#"><i class="fab fa-instagram"></i></a>
+     
+                <a href="#"><i class="fab fa-twitter-square"></i></a>
+    </div>
+</div>
