40697620d3ac06c0eb2775c48fd11fd99d590d62
damien-simplon <damien.bellec12@gmail.com>
2019-08-27 10:59:31 +0200

    html/css de la page produit fait

--- produit.php
+++ produit.php
@@ -3,15 +3,37 @@
   include "include/header.inc.php";
 ?>
 
-
 <img src="./img/banniere_sweat.jpg" class="img-fluid" alt="Bannière sweat">
 
 
-<div id="produit_main_image">
+<div id="produit_image_main">
 
-  <img src="./img/img_modif/sweat_front.jpg" class="img-thumbnail" alt="Responsive image">
+  <img src="./img/img_modif/sweat_front.jpg" class="img-thumbnail" alt="">
 
 </div>
+
+<div id="produit_image">
+
+  <div class="produit_image_miniature">
+    <img src="./img/img_modif/sweat_front.jpg" class="img-thumbnail" alt="">
+  </div>
+
+  <div class="produit_image_miniature">
+    <img src="./img/img_modif/sweat_back.jpg" class="img-thumbnail" alt="">
+  </div>
+
+  <div class="produit_image_miniature">
+    <img src="./img/img_modif/sweat_mvmt.jpg" class="img-thumbnail" alt="">
+  </div>
+
+</div>
+
+<div id="produit_description">
+  <p>
+  Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+  </p>
+</div>
+
 
 <?php
   include "include/footer.inc.html";
