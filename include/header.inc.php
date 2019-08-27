9c345d4178aef9eb19205d5a9ded887fce9cc9c7
Thomas Vasseur <tpjvasseur@gmail.com>
2019-08-27 16:25:59 +0200

    Modif Index + Footer + CSS

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,6 +1,5 @@
-<header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-
+<header class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -34,8 +33,5 @@
                 </li>                
             </ul>
         </div>
-
-        
     </nav>
-
 </header>
