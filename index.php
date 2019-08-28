ec3d1bb0df7f1ff5f01ad61629c1070a2cc7ed7e
Thomas Vasseur <tpjvasseur@gmail.com>
2019-08-28 11:40:30 +0200

    Ajout pages catalogues avec header

--- index.php
+++ index.php
@@ -6,7 +6,51 @@
     </head>
 
     <body>
-        <?php include "./include/header.inc.php"; ?>
+        <header class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <h2>SweatMore</h2>
+
+                <?php if (isset($_SESSION['user-id'])){ ?>
+                <ul class="navbar-nav d-flex flex-row" id="connexion_inscription">
+                    <li class="nav-item p-2"><a class="nav-link" href="./compte.php">Mon compte</a></li>
+                    <li class="nav-item p-2"><a class="nav-link" href="./deconnexion.php">Deconnexion</a></li>
+                </ul>
+
+                <?php } else { ?> 
+                <ul class="navbar-nav d-flex flex-row" id="connexion_inscription">
+                    <li class="nav-item p-2"><a class="nav-link" href="./connexion.php">Connexion</a></li>
+                    <li class="nav-item p-2"><a class="nav-link" href="./inscription.php">Inscription</a></li>
+                </ul>
+                <?php } ?>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./sweat/catalogue.php">Sweat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./tshirt/catalogue.php">T-shirt</a>
+                        </li>                
+                        <li class="nav-item">
+                            <a class="nav-link" href="./baskets/catalogue.php">Baskets</a>
+                        </li>                
+                        <li class="nav-item">
+                            <a class="nav-link" href="./homme/catalogue.php">Homme</a>
+                        </li>                
+                        <li class="nav-item">
+                            <a class="nav-link" href="./femme/catalogue.php">Femme</a>
+                        </li>                
+                    </ul>
+                </div>
+            </nav>
+        </header>
 
         <div id="content">
             <div class="container-fluid">
