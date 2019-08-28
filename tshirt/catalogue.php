76d3ca528027319a9a1f2fcc6cb3a6452f77adfe
lepretre <benjamin.lepretre.pro@gmail.com>
2019-08-28 11:59:57 +0200

    responsive page catalogue

--- tshirt/catalogue.php
+++ tshirt/catalogue.php
@@ -62,28 +62,28 @@
 
         <div class="container">
             <div class="row">
-                <div class="card col-md-3" style="margin-top:10px;">
+                <div class="card col-lg-3 col-md-3 col-sm-6" style="margin-top:10px;">
                     <img src="https://i2.cdscdn.com/pdt2/7/6/2/1/700x700/mp07228762/rw/3d-pullover-sweat-a-capuche-homme-femme-couple-cos.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
 
-                <div class="card col-md-3" style="margin-top:10px;">
+                <div class="card col-lg-3 col-md-3 col-sm-6" style="margin-top:10px;">
                     <img src="https://i2.cdscdn.com/pdt2/7/6/2/1/700x700/mp07228762/rw/3d-pullover-sweat-a-capuche-homme-femme-couple-cos.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
 
-                <div class="card col-md-3" style="margin-top:10px;">
+                <div class="card col-lg-3 col-md-3 col-sm-6" style="margin-top:10px;">
                     <img src="https://i2.cdscdn.com/pdt2/7/6/2/1/700x700/mp07228762/rw/3d-pullover-sweat-a-capuche-homme-femme-couple-cos.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
 
-                <div class="card col-md-3" style="margin-top:10px;">
+                <div class="card col-lg-3 col-md-3 col-sm-6" style="margin-top:10px;">
                     <img src="https://i2.cdscdn.com/pdt2/7/6/2/1/700x700/mp07228762/rw/3d-pullover-sweat-a-capuche-homme-femme-couple-cos.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
