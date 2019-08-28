9d5a0f12379989961b460daaf8f245680e5b5796
damien-simplon <damien.bellec12@gmail.com>
2019-08-28 10:46:42 +0200

    slideshow en js fait

--- index.php
+++ index.php
@@ -12,7 +12,10 @@
                 <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="./img/slide/tenue_complete_women_mvmt.jpg" class="d-block w-100" alt="1">
+                            <img src="" class="d-block w-100" alt="1">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./img/slide/tenue_complete_women_mvmt.jpg" class="d-block w-100" alt="2">
                         </div>
                         <div class="carousel-item">
                             <img src="./img/slide/basket_mvmt.jpg" class="d-block w-100" alt="2">
@@ -27,11 +30,11 @@
                             <img src="./img/slide/tshirt_mvmt.jpg" class="d-block w-100" alt="5">
                         </div>
                     </div>
-                    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                    <a class="carousel-control-prev" role="button" data-slide="prev">
                         <span class="fa fa-chevron-left text-dark" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                    <a class="carousel-control-next" role="button" data-slide="next">
                         <span class="fa fa-chevron-right text-dark" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                     </a>
@@ -59,7 +62,5 @@
     </div>
 
         <script type="text/javascript" src="./js/index.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -6,11 +6,48 @@ const newsletter = document.getElementById("popup_newsletter");
 const iconeNewsletter = newsletter.getElementsByTagName("i");
 const closeNewsletter = iconeNewsletter[0];
 
+const carousel = document.getElementById("carousel");
+const recupMainImage1 = carousel.getElementsByClassName("active")
+const recupMainImage2 = recupMainImage1[0].getElementsByTagName("img");
+const mainImage = recupMainImage2[0];
+const images = carousel.getElementsByTagName("img");
+const taille = images.length;
+const arrow = carousel.getElementsByTagName("a");
+var imageId = 1;
+stop = false;
 
 window.onload = function(){
+    mainImage.src = images[imageId].getAttribute("src");
     setTimeout(function(){
         newsletter.style.display = "block";
+        newsletter.style.zIndex = "100";
     }, 60000);
+    intervalle();
+}
+
+function intervalle(){
+    var stopIntervalle = setInterval(function(){
+        if(stop==true){
+            clearInterval(stopIntervalle);
+            stop = false;
+            intervalle();
+        }
+        else{
+            imageId++;
+            affichage();
+        }
+    },5000);
+}
+
+function affichage(){
+    stop = true;
+    if(imageId<=0){
+        imageId = taille-1;
+    }
+    else if(imageId>=taille){
+        imageId = 1;
+    }
+    mainImage.src = images[imageId].getAttribute("src");
 }
 
 closeCookie.onclick = function(){
@@ -19,4 +56,16 @@ closeCookie.onclick = function(){
 
 closeNewsletter.onclick = function(){
     newsletter.style.display = "none";
+}
+
+arrow[0].onclick = function(){
+    imageId--;
+    stop = true;
+    affichage();
+}
+
+arrow[1].onclick = function(){
+    imageId++;
+    stop = true;
+    affichage();
 }
