0be5c078097298fa26346728ed2b9511d340d425
Justine Lemaire <lemairejustine01@gmail.com>
2019-08-28 14:34:18 +0200

    pages footer css modifié html aussi

--- contact.php
+++ contact.php
@@ -1,15 +1,82 @@
-<?php 
-        include 'include/head.inc.html';
-?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <?php include "./include/head.inc.html"; ?>
+        <title>
+        Contact - SWEATMORE.FR
+        </title>
+    </head>
 
+    <body>
+    <header class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-<div id="formBanniere">
-        <form>
-                <div id="form">
                 <h2>SweatMore</h2>
-                        <span class="contact">Contactez-nous</span>
 
-                        <div id="insertForm"></div>
+                <?php if (isset($_SESSION['user-id'])){ ?>
+                <ul class="navbar-nav d-flex flex-row" id="connexion_inscription">
+                    <li class="nav-item p-2"><a class="nav-link" href="./compte.php">Mon compte</a></li>
+                    <li class="nav-item p-2"><a class="nav-link" href="./deconnexion.php">Deconnexion</a></li>
+                </ul>
+
+                <?php } else { ?> 
+                <ul class="navbar-nav d-flex flex-row" id="connexion_inscription">
+                    <li class="nav-item p-2"><a class="nav-link" href="./connexion.php">Connexion</a></li>
+                    <li class="nav-item p-2"><a class="nav-link" href="./inscription.php">Inscription</a></li>
+                </ul>
+                <?php } ?>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="./index.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./sweat/catalogue.php">Sweat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./tshirt/catalogue.php">T-shirt</a>
+                        </li>                
+                        <li class="nav-item">
+                            <a class="nav-link" href="./baskets/catalogue.php">Baskets</a>
+                        </li>                
+                        <li class="nav-item">
+                            <a class="nav-link" href="./homme/catalogue.php">Homme</a>
+                        </li>                
+                        <li class="nav-item">
+                            <a class="nav-link" href="./femme/catalogue.php">Femme</a>
+                        </li>                
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        
+
+		    <!-- la page concernée, faire ressortir l'onglet -->
+	<div class="containerNavContact">
+    <h3>Informations légales</h3>
+        
+        <div class="list-footer-contact">
+            <ul id="list-lien-footer-contact">
+               <button> <li><a href="contact.php" class="lien-footer">Contact</a></li></button> 
+               <button>  <li><a href="apropos.php" class="lien-footer">À propos</a></li></button> 
+               <button> <li><a href="mentions.php" class="lien-footer">Mentions légales</a></li></button> 
+               <button> <li><a href="cgv.php" class="lien-footer">CGV</a></li></button> 
+               <button> <li><a href="faq.php" class="lien-footer">Questions fréquentes</a></li><br></button> 
+            </ul>
+        </div>
+    </div>
+
+
+	<div id="formBanniere"></div>
+        <form>
+		<div id="form">
+		<span class="contact">Contactez-nous</span>
+
+			<div id="insertForm">
 
 				<div class="nomEmail" data-validate="Name and Email are required">
 					<span class="name">Votre nom</span>
@@ -21,34 +88,37 @@
 					<span class="focus-input100"></span>
 				</div>
 
-                                <div class="subject">
-                                        <span class="label-input100">Sujet</span>
-					<input class="input100" type="text" name="sujet" placeholder="Entrez votre sujet">
-					<span class="focus-input100"></span>
-                                </div>
+				<div class="subject">
+						<span class="label-input100">Sujet</span>
+						<input class="input100" type="text" name="sujet" placeholder="Entrez votre sujet">
+						<span class="focus-input100"></span>
+				</div>
                                 
-                                <div id="message">
-                                <label for="msg">Message :</label>
-                                        <textarea id="msg" name="user_message"></textarea>
-                                </div>
+				<div id="message">
+					<label for="msg">Message :</label>
+					<textarea id="msg" name="user_message"></textarea>
+				</div>
 
 				<div class="submit">
 					<button class="contact100-form-btn">
 						<span>
-                                               Envoyer
-                                                <i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							Envoyer
+							<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
 						</span>
 					</button>
-                                </div>
-			</form>
+				</div>
 
-			<span class="contactMore">
-				Pour toute question, veuillez vous adresser à notre centre d'appel :<span class="contact100-more-highlight"> +33 345 6889</span>
-			</span>
-		
-	</div>
-                        </div>
+				<span class="contactMore">
+					Pour toute question, veuillez vous adresser à notre centre d'appel :<span class="contact100-more-highlight"> +33 345 6889</span>
+				</span>
+			</div>
+		</div>
+	</form>
 
-                </div>
-        </form>
-</div>
+
+
+    <script type="text/javascript" src="./js/index.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
