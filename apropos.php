bb84d3bc98c6391933cbd58c1d579234c4cb6264
Justine Lemaire <lemairejustine01@gmail.com>
2019-08-28 16:24:59 +0200

    footer font size modif + à propos page Php à faire

--- apropos.php
+++ apropos.php
@@ -53,7 +53,7 @@
                 </div>
             </nav>
         </header>
-        
+<div class="pageApropos">
     <!-- la page concernée, faire ressortir l'onglet -->
     <div class="containerNavApropos">
         <h3>Informations légales</h3>
@@ -68,11 +68,25 @@
             </ul>
         </div>
     </div>
+                </div>
 
     <div class="pageClicApropos">
-</div>
+        <h2>Qui sommes-nous?</h2>
+            <p>SweatMore, entreprise leader sur le marché du sport, regroupe deux activités :
+                la création de produits sportifs et leur distribution en ligne et en magasins.</p><br>
+            <p>Basé sur un modèle intégré, Decathlon maîtrise toutes les étapes de vie de ses produits :
+            de la recherche et développement à la vente en passant par la conception, la production, et la logistique. 
+            Présente en France et à l’international avec plus de mille points de vente, les équipes des services, marques et magasins travaillent depuis 1976 avec une ambition constante:
+            innover dans tous les domaines pour rester le principal partenaire de jeu de tous les sportifs.</p>
+            <a href="#"><p>voir plus..</p><a><br>
+           <div id="contactPageClicApropos">
+                <p> <i class="fa fa-phone" aria-hidden="true"> Nous sommes joignable au:  0969 368 369<br>
+                 (N° Cristal, prix d'un appel local depuis un téléphone fixe), <br>du lundi au samedi de 9h à 20h. -</i><br>
+                 <br>Adresse <p> SWEATMORE SAS  Service Relation Clients<br> - 4 boulevard de Mons, 59650 Villeneuve d’Ascq</p>
+            </div>
+    </div>
       
- 
+    <?php include "./include/footer.inc.html"; ?>
     <script type="text/javascript" src="./js/index.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
