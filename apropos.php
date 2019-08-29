7c6e75ccd0e647a3a9033b80e209bc26ab660111
damien-simplon <damien.bellec12@gmail.com>
2019-08-29 14:08:23 +0200

    les pages php à la racine du dossier sont désormais purifiés des scripts Bootstrap qui entravaient mon script, c'était inacceptable j'ai donc dût agir pour le bien être de notre projet

--- apropos.php
+++ apropos.php
@@ -43,8 +43,6 @@
     </div>
       
     <?php include "./include/footer.inc.html"; ?>
-    <script type="text/javascript" src="./js/index.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
 </body>
 </html>
--- compte.php
+++ compte.php
@@ -81,8 +81,6 @@ session_start();
         <i class="fa fa-window-close-o" aria-hidden="true"></i>
     </div>
 
-        <script type="text/javascript" src="./js/index.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
     </body>
 </html>
